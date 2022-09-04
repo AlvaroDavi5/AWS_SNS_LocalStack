c3071b8664073cae071b6796eb5e4cce0da036ab
AlvaroDavi5 <alvaro-davi1@hotmail.com>
2022-09-04 14:57:15 -0300

    fix: updated SNS protocol from 'email' to 'sqs'

--- scripts/publish.js
+++ scripts/publish.js
@@ -10,7 +10,7 @@ const data = {
 };
 
 publishMessage(
-	'email',
+	'sqs',
 	`${topicFullArn}${topicName}`,
 	topicName,
 	data,
--- scripts/subscribe.js
+++ scripts/subscribe.js
@@ -3,9 +3,10 @@ const { subscribeTopic } = require('../src/sns.js');
 
 const topicFullArn = process.env.TOPIC_FULL_ARN || 'arn:aws:sns:us-east-1:000000000000:';
 const topicName = process.env.TOPIC_NAME || 'DEFAULT_TOPIC.fifo';
+const destiny = process.env.QUEUE_URL || 'http://localhost:4566/000000000000/DEFAULT_QUEUE.fifo';
 
 subscribeTopic(
-	'email',
+	'sqs',
 	`${topicFullArn}${topicName}`,
-	'alvaro.davisa@gmail.com',
+	destiny,
 );
