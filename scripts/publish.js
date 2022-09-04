fb345e39f764bd692973bd8507af7e9d0f7abfd7
AlvaroDavi5 <alvaro-davi1@hotmail.com>
2022-09-04 16:20:24 -0300

    feat: dynamic destiny and protocol

--- scripts/publish.js
+++ scripts/publish.js
@@ -3,6 +3,7 @@ const { publishMessage } = require('../src/sns.js');
 
 const topicFullArn = process.env.TOPIC_FULL_ARN || 'arn:aws:sns:us-east-1:000000000000:';
 const topicName = process.env.TOPIC_NAME || 'DEFAULT_TOPIC.fifo';
+const protocol = process.env.TOPIC_PROTOCOL || 'sqs';
 
 const data = {
 	subject: 'Hii',
@@ -10,7 +11,7 @@ const data = {
 };
 
 publishMessage(
-	'sqs',
+	protocol,
 	`${topicFullArn}${topicName}`,
 	topicName,
 	data,
--- scripts/subscribe.js
+++ scripts/subscribe.js
@@ -3,10 +3,11 @@ const { subscribeTopic } = require('../src/sns.js');
 
 const topicFullArn = process.env.TOPIC_FULL_ARN || 'arn:aws:sns:us-east-1:000000000000:';
 const topicName = process.env.TOPIC_NAME || 'DEFAULT_TOPIC.fifo';
-const destiny = process.env.QUEUE_URL || 'http://localhost:4566/000000000000/DEFAULT_QUEUE.fifo';
+const protocol = process.env.TOPIC_PROTOCOL || 'sqs';
+const destiny = process.env.TOPIC_DESTINY || 'http://localhost:4566/000000000000/DEFAULT_QUEUE.fifo';
 
 subscribeTopic(
-	'sqs',
+	protocol,
 	`${topicFullArn}${topicName}`,
 	destiny,
 );
