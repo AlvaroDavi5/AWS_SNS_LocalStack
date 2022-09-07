a7c51be78911e2bb7e62b6b63f3b2628fbcba3a7
AlvaroDavi5 <alvaro-davi1@hotmail.com>
2022-09-07 01:35:11 -0300

    doc: updated shell script

--- SNS_sample.sh
+++ SNS_sample.sh
@@ -8,26 +8,26 @@ awslocal --endpoint-url=http://localhost:4566 sns create-topic --name=notificati
 awslocal --endpoint-url=http://localhost:4566 sqs create-queue --queue-name=test_queue --region=us-east-1 --output=table | cat
 
 # list SNS topic
-awslocal sns list-topics
+awslocal sns list-topics --output=table | cat
 
 # list SQS queue
-awslocal sqs list-queues
+awslocal sqs list-queues --output=table | cat
 
 # subscribe SNS topic on SQS queue
 awslocal --endpoint-url=http://localhost:4566 sns subscribe --topic-arn=arn:aws:sns:us-east-1:000000000000:notification_events --protocol=sqs --notification-endpoint=http://localstack:4566/000000000000/test_queue --output=table | cat
 
 # publish message on SNS topic
-awslocal sns publish --endpoint-url=http://localhost:4566 --topic-arn=arn:aws:sns:us-east-1:000000000000:notification_events --message="Hello World" --region=us-east-1 --output=json | cat
+awslocal sns publish --endpoint-url=http://localhost:4566 --topic-arn=arn:aws:sns:us-east-1:000000000000:notification_events --message="Hello World" --region=us-east-1 --output=table | cat
 
 # receive message from SQS queue
 awslocal --endpoint-url=http://localhost:4566 sqs receive-message --queue-url=http://localhost:4566/000000000000/test_queue --region=us-east-1 --output=json | cat
 
 # delete message from SQS queue
-awslocal sqs delete-message --endpoint-url=http://localhost:4566 --queue-url=http://localhost:4566/000000000000/test_queue --region=us-east-1  --receipt-handle=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+awslocal sqs delete-message --endpoint-url=http://localhost:4566 --queue-url=http://localhost:4566/000000000000/test_queue --region=us-east-1  --receipt-handle=XXXXXXXXXX --output=table | cat
 
 # delete SNS topic
-awslocal sns delete-topic --topic-arn=arn:aws:sns:us-east-1:000000000000:notification_events
+awslocal sns delete-topic --topic-arn=arn:aws:sns:us-east-1:000000000000:notification_events --output=table | cat
 
 # delete SQS queue
-awslocal sqs delete-queue --queue-url=http://localhost:4566/000000000000/test_queue
+awslocal sqs delete-queue --queue-url=http://localhost:4566/000000000000/test_queue --output=table | cat
 
